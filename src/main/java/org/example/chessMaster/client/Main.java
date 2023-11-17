2b318073dd1f343ce6a0b7f5f525342fa3e0ab9b
Michal Andrejczuk <michal032@wp.pl>
2023-11-17 19:57:18 +0100

    remove comments

--- src/main/java/org/example/chessMaster/client/Main.java
+++ src/main/java/org/example/chessMaster/client/Main.java
@@ -13,35 +13,6 @@ public class Main {
          if (!gameWinner) gameOn = game.gameMenu();
         }
 
-
-//        Board board = new Board();
-//        Pawn pawn1 = new Pawn("a2", Color.WHITE);
-//        Pawn pawn2 = new Pawn("c2", Color.WHITE);
-//        Pawn pawn3 = new Pawn("a7", Color.BLACK);
-//        Pawn pawn4 = new Pawn("b7", Color.BLACK);
-//        board.setPieceOnBoard(pawn1);
-//        board.setPieceOnBoard(pawn2);
-//        board.setPieceOnBoard(pawn3);
-//        board.setPieceOnBoard(pawn4);
-//        board.showBoard();
-//        board.movePawn("a2", "a4");
-//        board.movePawn("a7", "a5");
-//        board.movePawn("c2", "c3");
-//        board.movePawn("b7", "b5");
-//        System.out.println();
-//        board.showBoard();
-
-//        Pawn[] enemyInTheRangeOfPawn = board.isEnemyInTheRangeOfPawn(pawn1);
-//        board.checkAndHitTheEnemy(enemyInTheRangeOfPawn, pawn1);
-//        enemyInTheRangeOfPawn = null;
-//        board.movePawn("a4", "a5");
-//        board.movePawn("c3", "c4");
-//        enemyInTheRangeOfPawn = board.isEnemyInTheRangeOfPawn(pawn4);
-//        board.checkAndHitTheEnemy(enemyInTheRangeOfPawn, pawn4);
-//
-//        board.showBoard();
-
-
     }
 
 }
